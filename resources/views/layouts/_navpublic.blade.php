f51bdd8a51873551604fc1bdcd07665702aad132
edocecchini01 <S1101827@studenti.univpm.it>
2023-05-12 16:28:19 +0200

    layout

---
+++ resources/views/layouts/_navpublic.blade.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="{{ route('catalog1') }}" title="Home">Catalogo</a></li>
+    <li><a href="{{ route('who') }}" title="Il nostro profilo aziendale">Chi siamo</a></li>
+    <li><a href="{{ route('where') }}" title="Dove trovarci">Dove Siamo</a></li>
+    <li><a href="mailto:info@acme.it" title="Mandaci un messaggio">Contattaci</a></li>
+</ul>

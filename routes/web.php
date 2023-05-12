20e65d228159f0f4310bcbf83d1b91d51eb628bc
Giansimone Coccia <S1097845@studenti.univpm.it>
2023-05-12 15:20:56 +0200

    Svuotamento

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,34 +13,6 @@ use App\Http\Controllers\UserController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [PublicController::class, 'showCatalog1'])
-        ->name('catalog1');
-
-Route::get('/selTopCat/{topCatId}', [PublicController::class, 'showCatalog2'])
-        ->name('catalog2');
-
-Route::get('/selTopCat/{topCatId}/selCat/{catId}', [PublicController::class, 'showCatalog3'])
-        ->name('catalog3');
-
-Route::get('/admin', [AdminController::class, 'index'])
-        ->name('admin');
-
-Route::get('/admin/newproduct', [AdminController::class, 'addProduct'])
-        ->name('newproduct');
-
-Route::post('/admin/newproduct', [AdminController::class, 'storeProduct'])
-        ->name('newproduct.store');
-
-Route::get('/user', [UserController::class, 'index'])
-        ->name('user')->middleware('can:isUser');
-
-
-Route::view('/where', 'where')
-        ->name('where');
-
-Route::view('/who', 'who')
-        ->name('who');
 
 /*  Rotte aggiunte da Breeze
 

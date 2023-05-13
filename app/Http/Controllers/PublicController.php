e320ff52c2473a68a644accc2375c5e0f0ceefc9
Laura Ferretti <s1098036@studenti.univpm.it>
2023-05-13 13:20:39 +0200

    controller

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UtenteModel;
 use Illuminate\Support\Facades\Log;
 
 
 class PublicController extends Controller
 {
+    protected $_utenteModel;
 
+    public function __construct() {
+        $this->_utenteModel = new UtenteModel;
+    }
+
+    public function showInfoUtente() {
+        
+        $data = "Vediamo se funziona";
+        //$user = $this->_utenteModel->getInfoUtente('UC0001');
+        //dd($user);
+
+        return view('prova')
+                    ->with('data', $data);
+    }
 }

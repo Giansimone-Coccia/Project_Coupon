85db04a1230c475c786a8adec80bc98f3bf559e3
Walter Di Sabatino <waldisab2001@gmail.com>
2023-05-13 09:33:24 +0200

    Update dettaglio_offerta.blade.php

--- resources/views/dettaglio_offerta.blade.php
+++ resources/views/dettaglio_offerta.blade.php
@@ -1,7 +1,19 @@
-<?php
+@extends('layouts.public')
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+@section('title', 'Dettaglio offerta')
 
+@section('content')
+<div class="container_dettaglio_offerta">
+    <div class="image">
+        <img src="{{ asset('images/companies/Conad-logo.png' ) }}" alt="Nome Offerta">
+    </div>
+    <div class="info">
+        <h2>CONAD 20% OFF</h2>
+        <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+        <div class="price">PROMOZIONE: sconto 20%</div>
+        <div class="price"  style="color:#CC0000">Data Scadenza: 20/08/2023</div>
+        <div class="price">Modalità di fruizione: Negozio fisico 20%</div>
+        <button> Riscatta </button>
+    </div>
+</div>
+@endsection

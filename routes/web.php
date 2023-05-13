e77e31cf0c990d61422b55130149de7809c75999
Walter Di Sabatino <waldisab2001@gmail.com>
2023-05-13 10:53:45 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,15 +33,22 @@ Route::view('/', 'start')
         ->name('start');
 
 Route::view('/FAQ', 'FAQ')
-        ->name('FAQ')
-        
+        ->name('FAQ');
+
 Route::view('/area_personale_utente', 'area_personale_utente')
         ->name('area_personale_utente');
 
 Route::view('/chi_siamo', 'chi_siamo')
         ->name('chi_siamo');
 
-;
+Route::view('/coupon', 'coupon')
+        ->name('coupon');
+
+Route::view('/dettaglio_offerta', 'dettaglio_offerta')
+        ->name('dettaglio_offerta');
+
+Route::view('/lista_coupon', 'lista_coupon')
+        ->name('lista_coupon');
 
 Route::view('/login', 'login')
         ->name('login');
@@ -49,10 +56,16 @@ Route::view('/login', 'login')
 Route::view('/modifica_profilo_utente', 'modifica_profilo_utente')
         ->name('modifica_profilo_utente');
 
-Route::view('/lista_coupon', 'lista_coupon')
-        ->name('lista_coupon');
+Route::view('/offerte_azienda', 'offerte_azienda')
+        ->name('offerte_azienda');
+
+Route::view('/register', 'register')
+        ->name('register');
+
+Route::view('/risultati_page', 'risultati_page')
+        ->name('risultati_page');
 
 
 
-Route::view('/coupon', 'coupon')
-        ->name('coupon');
+
+

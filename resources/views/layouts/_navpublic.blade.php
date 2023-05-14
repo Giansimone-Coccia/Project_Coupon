c91625af598b4d288cb2c7f50e5bd041694ec1c2
edocecchini01 <S1101827@studenti.univpm.it>
2023-05-14 11:36:27 +0200

    navpublic

--- resources/views/layouts/_navpublic.blade.php
+++ resources/views/layouts/_navpublic.blade.php
@@ -1,24 +1,27 @@
-<ul class= "left listnone">
-    <li class="bar-item vectorlogo" >
+<ul class="left listnone">
+    <li class="bar-item vectorlogo">
         <img src="{{ asset('images/logo/coupon-logo.svg' ) }}" alt="Logo" class="width100 height100" onclick="window.location.href = '{{ route('start') }}';">
     </li>
-    <!--@auth
-    <li class="button bar-item right" onclick="window.location.href = '/';">Profilo</li>
-    <li class="button bar-item right" onclick="window.location.href = '{{ route('logout') }}';">Logout</li>
+    <!--
+    @auth
+        <li class="button bar-item right" onclick="window.location.href = '{{ route('area_personale_utente') }}';">Area personale</li>
+        <li class="button bar-item right" onclick="window.location.href = '{{ route('logout') }}';">Logout</li>
     @endauth
     @guest
+        <li class="button bar-item right" onclick="window.location.href = '{{ route('login') }}';">Login</li>
+    @endguest
+    -->
+    <li class="button bar-item right" onclick="window.location.href = '{{ route('area_personale_utente', ['UC0001']) }}';">Area personale</li>
     <li class="button bar-item right" onclick="window.location.href = '{{ route('login') }}';">Login</li>
-    @endguest-->
-    <li class="button bar-item right" onclick="window.location.href = '{{ route('login') }}';">Login</li>
-    <li class="button bar-item  right" onclick="window.location.href = '{{ route('FAQ') }}';">FAQ</li>
-    <li class="button bar-item  right" onclick="window.location.href = '{{ route('chi_siamo') }}';">Chi siamo</li>
+    <li class="button bar-item right" onclick="window.location.href = '{{ route('FAQ') }}';">FAQ</li>
+    <li class="button bar-item right" onclick="window.location.href = '{{ route('chi_siamo') }}';">Chi siamo</li>
 </ul>
-<ul class= "right listnone"> 
+<ul class="right listnone"> 
     <li class="bar-item max-heigth52" >
-        <input placeholder="Search.." class ="searchbar height36" name="search">
+        <input placeholder="Search.." class="searchbar height36" name="search">
     </li>
     <li class="button bar-item" >
-        <img src="{{ asset('images/icons/search_icon.svg' ) }}"  alt="Search" class= " width100">
+        <img src="{{ asset('images/icons/search_icon.svg' ) }}" alt="Search" class="width100">
     </li>
 </ul>
 

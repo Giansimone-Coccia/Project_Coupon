a13d014001b8f725c7f0884f69ba57aefd604138
Walter Di Sabatino <waldisab2001@gmail.com>
2023-05-14 16:12:16 +0200

    Update public.blade.php

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -34,26 +34,27 @@
 
     <!-- end #content -->
     <footer>
-        <div class="footer-content">
-            <br>
-            <img src="{{ asset('images/logo/coupon-logo.svg') }}" alt="logo" width="150" height="150">
-            <p>Coupon4U è il sito adatto per ricercare tra milioni di offerte. Scegli quella giusta per te e richiedi l'emissioni di un coupon da presentare in uno dei punti vendita aderenti all'iniziativa.</p>
-            <br>
-            <ul>
-                <li><p>Tel: +39 3541897526</p></li>
-                <li><p>FAX: +39 0541278</p></li>
-                <li><p>E-mail: info@Coupon4U.it</p></li>
-            </ul>
-        </div>
-        <div class="footer-bottom">
-            <p>Copyright 2023. All Rights Reserved. Coupon4U </p>
-            <div class="footer-menu">
-                <ul class="f-menu">
-                    <li><a onclick="window.location.href = '{{ route('start') }}';">Home</a></li>
-                    <li><a onclick="window.location.href = '{{ route('chi_siamo') }}';">About</a></li>
+        
+            <div class="footer-content padding-top-16">
+
+                <img src="{{ asset('images/logo/coupon-logo.svg') }}" alt="logo" width="150" height="150">
+                <p>Coupon4U è il sito adatto per ricercare tra milioni di offerte. Scegli quella giusta per te e richiedi l'emissioni di un coupon da presentare in uno dei punti vendita aderenti all'iniziativa.</p>
+                <br>
+                <ul>
+                    <li><p>Tel: +39 3541897526</p></li>
+                    <li><p>FAX: +39 0541278</p></li>
+                    <li><p>E-mail: info@Coupon4U.it</p></li>
                 </ul>
             </div>
-        </div>
+            <div class="footer-bottom">
+                <p>Copyright 2023. All Rights Reserved. Coupon4U </p>
+                <div class="footer-menu">
+                    <ul class="f-menu">
+                        <li><a onclick="window.location.href = '{{ route('start') }}';">Home</a></li>
+                        <li><a onclick="window.location.href = '{{ route('chi_siamo') }}';">About</a></li>
+                    </ul>
+                </div>
+            </div>
 
     </footer>
     <!-- end #footer -->

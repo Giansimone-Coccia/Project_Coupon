14d51d72d353beb618d77a9e1effa0dcf107b6a2
Giansimone Coccia <S1097845@studenti.univpm.it>
2023-05-14 14:54:32 +0200

    Modifiche dettaglio offerta

--- resources/views/dettaglio_offerta.blade.php
+++ resources/views/dettaglio_offerta.blade.php
@@ -5,16 +5,18 @@
 @section('content')
 <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
 
+@isset($dettaglio_offerta)
+@forach($dettaglio_offerta as $offerta)
 <div class="container_dettaglio_offerta">
     <div class="image">
         <img src="{{ asset('images/companies/Conad-logo.png' ) }}" alt="Nome Offerta">
     </div>
     <div class="info">
-        <h2>CONAD 20% OFF</h2>
-        <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
-        <div class="price">PROMOZIONE: sconto 20%</div>
-        <div class="price"  style="color:#CC0000">Data Scadenza: 20/08/2023</div>
-        <div class="price">Modalità di fruizione: Negozio fisico 20%</div>
+        <h2>$offerta->nomeOff</h2>
+        <p>$offerta->oggettoOff</p>
+        <div class="price">PROMOZIONE: </div>
+        <div class="price"  style="color:#CC0000">Data Scadenza: </div>
+        <div class="price">Modalità di fruizione: $offerta->modalita</div>
         <button> Riscatta </button>
     </div>
 </div>

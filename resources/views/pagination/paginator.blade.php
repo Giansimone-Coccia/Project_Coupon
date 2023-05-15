e510470341cf4693e51ec3ac7483cc11bd87c3ad
Walter Di Sabatino <waldisab2001@gmail.com>
2023-05-15 16:55:35 +0200

    Creazione Paginator

---
+++ resources/views/pagination/paginator.blade.php
@@ -0,0 +1,35 @@
+@if ($paginator->lastPage() != 1)
+<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
+
+<div id="pagination" class = "margin-top-10 center">
+    {{ $paginator->firstItem() }} - {{ $paginator->lastItem() }} di {{ $paginator->total() }} ---
+
+    <!-- Link alla prima pagina -->
+    @if (!$paginator->onFirstPage())
+        <a href="{{ $paginator->url(1) }}">Inizio</a> |
+    @else
+        Inizio |
+    @endif
+
+    <!-- Link alla pagina precedente -->
+    @if ($paginator->currentPage() != 1)
+        <a href="{{ $paginator->previousPageUrl() }}">&lt; Precedente</a> |
+    @else
+        &lt; Precedente |
+    @endif
+
+    <!-- Link alla pagina successiva -->
+    @if ($paginator->hasMorePages())
+        <a href="{{ $paginator->nextPageUrl() }}" class=" ">Successivo &gt;</a> |
+    @else
+        Successivo &gt; |
+    @endif
+
+    <!-- Link all'ultima pagina -->
+    @if ($paginator->hasMorePages())
+        <a href="{{ $paginator->url($paginator->lastPage()) }}">Fine</a>
+    @else
+        Fine
+    @endif
+</div>
+@endif

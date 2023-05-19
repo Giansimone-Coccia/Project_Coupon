7d7952c0d45142ab8ec1e3c3c6b3552bea9fb4ca
Laura Ferretti <s1098036@studenti.univpm.it>
2023-05-19 16:17:21 +0200

    f

--- resources/views/modifica_faq.blade.php
+++ resources/views/modifica_faq.blade.php
@@ -1,7 +1,43 @@
-<?php
+@extends('layouts.public')
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+@section('title', 'Crea faq')
 
+@section('content')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
+
+    <div class="creazioneOfferta">
+
+            {{ Form::open(array('route' => 'crea_faq.store', 'class' => 'productForm')) }}
+            <h1>Crea FAQ</h1>
+
+                {{ Form::label('domanda', 'Domanda', ['class' => 'label-input']) }}
+                {{ Form::text('domanda', '', ['class' => 'input', 'id' => 'domanda']) }}
+                @if ($errors->first('domanda'))
+                <ul class="errors">
+                    @foreach ($errors->get('domanda') as $message)
+                    <li>{{ $message }}</li>
+                    @endforeach
+                </ul>
+                @endif
+    
+
+         
+                {{ Form::label('risposta', 'Risposta', ['class' => 'label-input']) }}
+                {{ Form::text('risposta', '', ['class' => 'input', 'id' => 'risposta']) }}
+                @if ($errors->first('risposta'))
+                <ul class="errors">
+                    @foreach ($errors->get('risposta') as $message)
+                    <li>{{ $message }}</li>
+                    @endforeach
+                </ul>
+                @endif
+          
+                          
+                {{ Form::submit('Registra', ['class' => "buttonOfferta"]) }}
+            
+            {{ Form::close() }}
+
+    </div>
+
+</div>
+@endsection

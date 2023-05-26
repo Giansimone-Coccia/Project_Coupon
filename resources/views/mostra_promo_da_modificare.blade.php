86be823eba0d270be195d286c94764a4545c0977
edocecchini01 <S1101827@studenti.univpm.it>
2023-05-26 16:33:59 +0200

    fix modifica form

--- resources/views/mostra_promo_da_modificare.blade.php
+++ resources/views/mostra_promo_da_modificare.blade.php
@@ -11,7 +11,7 @@
 
 <div class="overlay" id="overlay"></div>
 
-<div id = "listaOfferte" class="main content padding" style="max-width: 1300px; margin-top: 150px;">
+<div id = "listaOfferte" class="main content padding" style="max-width: 1300px; margin-top: 70px;">
 
     <div class="couponlist" align="center">
         <h1 style="font-weight: 300">Lista delle offerte dell'azienda {{$azienda->nome}}:</h1>
@@ -20,7 +20,7 @@
         @foreach($allOfferteAzienda as $offerta)
         <div class="coupon" onclick="window.location.href = '{{ route('dettaglio_offerta', [$azienda->id,$offerta->id]) }}';" >
             <img src="{{ asset('images/products/' . $offerta->logoOff) }}" class ="rounded-corners" alt="Logo offerta">
-            <h3><span>Nome azienda:</span>{{$offerta->nomeOff}}</h3>
+            <h3><span>Nome offerta:</span>{{$offerta->nomeOff}}</h3>
             <ul>
                 <button class="pulsanti_staff" onclick="window.location.href = '{{ route('modifica_offerta', [$offerta->id]) }}'; event.stopPropagation();" > Modifica </button>
                 <button id="deleteBtn" class="pulsanti_staff" onclick=" event.stopPropagation();"> Elimina </button>

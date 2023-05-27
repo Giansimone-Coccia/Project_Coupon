8272e44b0f51ad4f4b1d79cd6a0d0dd0e308eb9c
Laura Ferretti <s1098036@studenti.univpm.it>
2023-05-27 16:05:49 +0200

    button

---
+++ resources/views/helpers/buttonAdmin.blade.php
@@ -0,0 +1,12 @@
+@php
+$currentUser = Auth::user();
+@endphp
+
+
+@can('isAdmin')
+<ul>
+    <button class="pulsanti_staff" onclick="window.location.href = '{{ route('modifica_azienda', [$azienda->id]) }}'; event.stopPropagation();"> Modifica </button>
+    <button id="deleteBtn" class="pulsanti_staff" onclick=" event.stopPropagation();"> Elimina </button>
+</ul>
+@endcan
+

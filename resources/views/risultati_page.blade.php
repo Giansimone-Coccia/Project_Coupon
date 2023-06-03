7a24e80e741b816d61f8af17c2b27757221c1bd7
Walter Di Sabatino <waldisab2001@gmail.com>
2023-06-03 12:18:08 +0200

    Update risultati_page.blade.php

--- resources/views/risultati_page.blade.php
+++ resources/views/risultati_page.blade.php
@@ -12,40 +12,38 @@
             <h4 style="font-weight: 500">{{$aziendaSearch}}<br>{{$descSearch}}</h4>
             @endisset
             @endisset
-            
+
             @isset($message)
             <h1 style="font-weight: 500">{{$message}}</h1>
             @endisset
-            
+
         </div>
-        
+
         @isset($promos)
-        
+
         <div class="container-azienda border-top-grey padding-top-16">
             <div class="row-offerte">
 
 
-                
+
                 @foreach ($promos as $promo)
 
-                <div class="quarter">
-                    <div class="single-offerta " onclick="window.location.href = '{{ route('dettaglio_offerta_ricerca', [$promo->id]) }}';">
-                        <div class="inline-block">
-                            <div class="centerimages">
-                                @include('helpers/productDate', ['dataScadenza' => $promo->tempoFruiz])
-                                <img class = "rounded-corners width100" src="{{ asset('images/products/' . $promo->logoOff) }}" alt="Offerta azienda"/>
-                            </div>
-                            <h3>{{$promo->nomeOff}}</h3>
-                        </div> 
-                    </div>
+                <div class="single-offerta quarter " onclick="window.location.href = '{{ route('dettaglio_offerta_ricerca', [$promo->id]) }}';">
+                    <div class="inline-block">
+                        <div class="centerimages">
+                            @include('helpers/productDate', ['dataScadenza' => $promo->tempoFruiz])
+                            <img class = "rounded-corners width100" src="{{ asset('images/products/' . $promo->logoOff) }}" alt="Offerta azienda"/>
+                        </div>
+                        <h3>{{$promo->nomeOff}}</h3>
+                    </div> 
                 </div>
 
                 @endforeach
-                
+
 
             </div>
         </div>
-        
+
         @include('pagination.paginator_ricerca', ['paginator' => $promos])
         @endisset
         <!-- End page content -->

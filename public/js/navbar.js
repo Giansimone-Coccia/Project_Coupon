3161fc0acb90fe790042986fefab32ec67a5a034
Walter Di Sabatino <waldisab2001@gmail.com>
2023-06-05 12:05:53 +0200

    Modifiche navbar.js e navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -9,13 +9,13 @@ $(document).ready(function () {
 function toggleElementVisibility() {
     var logo = $('.logo');
     var searchbar = $('.search');
-    if ($(window).width() < 950) { // Larghezza desiderata per nascondere l'elemento
+    if ($(window).width() < 975) { // Larghezza desiderata per nascondere l'elemento
         logo.hide();
     } else {
         logo.show();
     }
 
-    if ($(window).width() < 640) { // Larghezza desiderata per nascondere l'elemento
+    if ($(window).width() < 700) { // Larghezza desiderata per nascondere l'elemento
         searchbar.hide();
     } else {
         searchbar.show();
--- resources/views/layouts/_navpublic.blade.php
+++ resources/views/layouts/_navpublic.blade.php
@@ -2,7 +2,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="{{ asset('js/navbar.js') }}"></script>
 
-<li class="vectorlogo right">
+<li class="vectorlogo right logo">
     <img src="{{ asset('images/logo/coupon-logo.svg' ) }}" alt="Logo" class="width100 height100" onclick="window.location.href = '{{ route('start') }}';">
 </li>
 
@@ -33,9 +33,9 @@
 
 </ul>
 
-<li class="bar-item" >
+<li class="bar-item search" >
     <form method="GET" action="{{ route('risultati_page') }}">
-        <button type="submit" class="button bar-item search-lens right "  >
+        <button type="submit" class="button bar-item search-lens right"  >
             <img src="{{ asset('images/icons/search_icon.svg' ) }}" alt="Search" class="width100">
         </button>
         <input type="text" name="azienda" placeholder="Cerca per Azienda"  class="searchbar height36" name="search" value="{{ request()->input('azienda') }}">

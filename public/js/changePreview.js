99f738cb5cc3d047a65bffcf3bd5fbd8bdb1086e
edocecchini01 <S1101827@studenti.univpm.it>
2023-06-05 19:06:06 +0200

    Update changePreview.js

--- public/js/changePreview.js
+++ public/js/changePreview.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
       reader.readAsDataURL(this.files[0]);
     });
   });
+
+  $(document).ready(function() {
+    $('#logoOff').change(function() {
+      var reader = new FileReader();
+      
+      reader.onload = function(e) {
+        $('#previewImage').attr('src', e.target.result);
+      }
+      
+      reader.readAsDataURL(this.files[0]);
+    });
+  });
